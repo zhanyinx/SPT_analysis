4b72accc8df47874d1b9a59ef8d35f361528eec2
zhanyinx <yinxiu.zhan@fmi.ch>
2021-06-28 14:20:59 +0200

    :pencil: Integrate output with extra information

--- source/msd_calculation/msd.py
+++ source/msd_calculation/msd.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import argparse
-
+import re
 
 from utils import *
 
@@ -36,14 +36,30 @@ def main():
     """Calculate MSD given trajectory file."""
     args = _parse_args()
     if os.path.isdir(args.input):
-        trajectory_files = glob.glob(f"{args.input}/*pure*csv")
+        trajectory_files = glob.glob(f"{args.input}/*_corrected.csv")
     elif os.path.isfile(args.input):
         trajectory_files = [args.input]
     else:
         raise ValueError(f"{args.input} must be a directory or a file!")
+
+    # Filter tracks based on quality (length, number of tracks)
+    filter_tracks(trajectory_files)
+
+    path = os.path.abspath(args.input)
+    trajectory_files = glob.glob(f"{path}/*pure.csv")
+
+    # Calculate all tamsd
     results = [calculate_all_tamsd(f) for f in trajectory_files]
     df = pd.concat(results)
 
+    # Add more info to results
+    allinfo = df["traj_file"]
+    df["cell_line"] = [re.search("laminin_2i_([^_]*)", x)[1] for x in allinfo]
+    df["induction_time"] = [
+        re.search("laminin_2i_[^_]*_([^_]*)_", x)[1] for x in allinfo
+    ]
+    df["rep"] = [re.search("_([0-9])_", x)[1] for x in allinfo]
+    df["stage"] = [re.search("_s([0-9])_", x)[1] for x in allinfo]
     df.to_csv(args.output, index=False)
 
 
