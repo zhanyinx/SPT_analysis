683fc72e1a6a38c9858c5ec3280d24e54123aafb
yinxiu zhan <yinxiuzhan@F346M-BD8E6A.local>
2021-06-29 18:29:03 +0200

    :pencil: Move options to sidebar

--- streamlit/visualize_msd.py
+++ streamlit/visualize_msd.py
@@ -8,10 +8,10 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+# Set title
 st.title("MSD visualisation app")
 
-
+# load data and cache it
 @st.cache
 def load_data(data: str):
     """Load data from csv file using pandas."""
@@ -22,43 +22,71 @@ def load_data(data: str):
     return df
 
 
-filename = st.text_input("Enter the input file containing the data", "./output.csv")
-interval = int(st.text_input("Time step acquisition", "10"))
-limit = int(st.text_input("Until where you trust the data?", "500"))
-
-
+# Take input from user and load file and make a copy
+filename = st.sidebar.text_input(
+    "Enter the input file containing the tamsd data.", "rad21_with_gaps.csv"
+)
 original_data = load_data(filename)
 data = original_data.copy()
 
-cell_lines = st.multiselect(
+# Take input from user for time resultion of acquisition
+interval = int(st.sidebar.text_input("Acquisition time resolution", "10"))
+data["lags"] = data["lags"] * interval
+
+# Select the upper limit for trustable data
+limit = int(
+    st.sidebar.slider(
+        "Until where you trust the data (in second)?",
+        min_value=0,
+        max_value=max(data["lags"]),
+        value=500,
+        step=interval,
+    )
+)
+
+# Select cell lines and induction time to show
+cell_lines = st.sidebar.multiselect(
     "Choose your cell lines (multiple)", data["cell_line"].unique()
 )
-induction_time = st.multiselect(
+induction_time = st.sidebar.multiselect(
     "Choose the induction times to keep", data["induction_time"].unique()
 )
 
-data["lags"] = data["lags"] * interval
+# Filter data to keep only the selected lines and induction time
 data = data[(data["lags"] < limit)]
 data = data[data["cell_line"].isin(cell_lines)]
 data = data[data["induction_time"].isin(induction_time)]
 
 
-data["condition"] = [
-    f"cl_{cl}_rep{rep}_time{time}"
-    for cl, rep, time in zip(data["cell_line"], data["rep"], data["induction_time"])
-]
+# Options for plot
+pool_replicates = st.sidebar.checkbox("Pool replicates")
+if pool_replicates:
+    data["condition"] = [
+        f"{cl}_itime{time}"
+        for cl, time in zip(data["cell_line"], data["induction_time"])
+    ]
+else:
+    data["condition"] = [
+        f"{d}_{cl}_time{time}"
+        for cl, d, time in zip(data["cell_line"], data["date"], data["induction_time"])
+    ]
 
-if st.checkbox("Show raw data"):
-    st.subheader("Raw data")
-    datahead = data.head()
-    st.write(datahead)
-
+# Plot
 fig = plt.figure()
-
-sns.lineplot(data=data, x="lags", y="tamsd", hue="condition", err_style="bars", ci=68)
+if st.checkbox("Plot standard deviation instead of 68 confidence interval"):
+    sns.lineplot(
+        data=data, x="lags", y="tamsd", hue="condition", err_style="bars", ci="sd"
+    )
+else:
+    sns.lineplot(
+        data=data, x="lags", y="tamsd", hue="condition", err_style="bars", ci=68
+    )
 plt.xscale("log")
 plt.yscale("log")
 plt.xlabel("dt (sec)")
 plt.ylabel("EA-tamsd (um^2)")
+plt.ylim(0.01, 2)
 
 st.pyplot(fig)
+
+# Create table of alphas and Ds
