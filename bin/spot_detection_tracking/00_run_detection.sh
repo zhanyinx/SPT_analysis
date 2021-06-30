539e07cac4c86ec0c3f414011cc9e7620e167904
yinxiu zhan <yinxiuzhan@F346M-BD8E6A.local>
2021-06-30 12:31:43 +0200

    :sparkles: Add input parameters to bash scripts for spot detection and tracking

--- bin/spot_detection_tracking/00_run_detection.sh
+++ bin/spot_detection_tracking/00_run_detection.sh
@@ -1,12 +1,90 @@
-path="PATH2Tiff"
+#!/bin/bash
+    
+## Author(s): Yinxiu Zhan
+## Contact: yinxiu.zhant@fmi.ch
+## This software is distributed without any guarantee under the terms of the GNU General
+## Public License, either Version 2, June 1991 or Version 3, June 2007.
+
+
+function usage {
+    echo -e "usage : 00_run_detection.sh -i INPUT -s PATHSPT [-n NGPU] [-h]"
+    echo -e "Use option -h|--help for more information"
+}
+
+function help {
+    usage;
+    echo 
+    echo "Detect spots using deepBlink."
+    echo "See https://github.com/BBQuercus/deepBlink for details about deepBlink"
+    echo "---------------"
+    echo "OPTIONS"
+    echo
+    echo "   -i|--input INPUT : input folder containing the tiff file of all movies you want to run spot detection."
+    echo "   -s|--scriptpath SCRIPTPATH : path to SPT analysis github folder path"
+    echo "   [-n|--ngpu NGPU] : number of GPUs to use, default 1"
+    echo "   [-h|--help]: help"
+    exit;
+}
+
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+      "--input") set -- "$@" "-i" ;;
+      "--ngpu")   set -- "$@" "-n" ;;
+      "--scriptpath")   set -- "$@" "-s" ;;
+      "--help")   set -- "$@" "-h" ;;
+       *)        set -- "$@" "$arg"
+  esac
+done
+
+pathtiff=""
+pathSPT=""
+ngpu=1
+
+while getopts ":i:n:s:h" OPT
+do
+    case $OPT in
+        i) pathtiff=$OPTARG;;
+        n) ngpu=$OPTARG;;
+        s) pathSPT=$OPTARG;;
+        h) help ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ $# -lt 4 ]
+then
+    usage
+    exit
+fi
+
+if ! [ -d $pathtiff ]; then
+    echo "$pathtiff does not exist!"
+    exit
+fi
+
+if ! [ -d $pathSPT ]; then
+    echo "$pathSPT does not exist!"
+    exit
+fi
 
 gpu=0
-ngpu=2
 
-for files in `ls $path/*tif`;do
+for files in `ls $pathtiff/*tif`;do
 
 	export CUDA_VISIBLE_DEVICES=$gpu
-	python ../../source/spot_detection_tracking/3d_spot_detection.py -i $files -m ../../models/210518_141754_real_add_all_pia_180521.h5 -su "um" -pw 0.16 -hw 0.16 -vd 0.3 -ti 10 > nohup.out 2>&1 &
+	python $pathSPT/source/spot_detection_tracking/3d_spot_detection.py -i $files -m $pathSPT/models/210518_141754_real_add_all_pia_180521.h5 -su "um" -pw 0.16 -hw 0.16 -vd 0.3 -ti 10 > nohup.out 2>&1 &
 	let gpu=gpu+1
 	if [ $gpu == $ngpu ]; then
 		gpu=0
---
+++ bin/spot_detection_tracking/01_tracking_cell_assignments_with_gaps.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+    
+## Author(s): Yinxiu Zhan
+## Contact: yinxiu.zhant@fmi.ch
+## This software is distributed without any guarantee under the terms of the GNU General
+## Public License, either Version 2, June 1991 or Version 3, June 2007.
+
+
+function usage {
+    echo -e "usage : 01_tracking_cell_assignments_with_gaps.sh -i INPUT -s PATHSPT -f PATHFIJI -m PATHMASK [-h]"
+    echo -e "Use option -h|--help for more information"
+}
+
+function help {
+    usage;
+    echo 
+    echo "Detect spots using deepBlink."
+    echo "See https://github.com/BBQuercus/deepBlink for details about deepBlink"
+    echo "---------------"
+    echo "OPTIONS"
+    echo
+    echo "   -i|--input INPUT : input folder containing the xml file of all movies you want to run spot detection."
+    echo "   -s|--scriptpath SCRIPTPATH : path to spt analysis github folder path"
+    echo "   -f|--fijipath FIJIPATH : path to executable fiji (ImageJ-...64)"
+    echo "   -m|--maskpath MASKPATH : path to masks used for cell id assignment"
+    echo "   [-h|--help]: help"
+    exit;
+}
+
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+      "--input") set -- "$@" "-i" ;;
+      "--fijipath")   set -- "$@" "-f" ;;
+      "--scriptpath")   set -- "$@" "-s" ;;
+      "--maskpath")   set -- "$@" "-m" ;;
+      "--help")   set -- "$@" "-h" ;;
+       *)        set -- "$@" "$arg"
+  esac
+done
+
+pathxml=""
+pathspt=""
+pathfiji=""
+pathmask=""
+
+while getopts ":i:m:f:s:h" OPT
+do
+    case $OPT in
+        i) pathxml=$OPTARG;;
+        f) pathfiji=$OPTARG;;
+        s) pathspt=$OPTARG;;
+        s) pathmask=$OPTARG;;
+        h) help ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ $# -lt 8 ]
+then
+    usage
+    exit
+fi
+
+if ! [ -d $pathxml ]; then
+    echo "$pathxml does not exist!"
+    exit
+fi
+
+if ! [ -d $pathspt ]; then
+    echo "$pathspt does not exist!"
+    exit
+fi
+
+if ! [ -d $pathfiji ]; then
+    echo "$pathfiji does not exist!"
+    exit
+fi
+
+if ! [ -d $pathmask ]; then
+    echo "$pathmask does not exist!"
+    exit
+fi
+
+if [ -f apporun.sh ]; then
+    rm apporun.sh
+fi
+
+if [ -f apporun1.sh ]; then
+    rm apporun1.sh
+fi
+
+
+if ! [ -d tracks_gap ]; then
+        mkdir tracks_gap
+fi
+
+for file in `ls $input/*xml`; do
+    echo "nice -n 19 $pathfiji --ij2 --headless --run $pathspt/source/spot_detection_tracking/trackmate_tracking_gap.py 'basedir=\"$basedir/tracks_gap\",xml=\"$file\"'" >> apporun.sh
+done
+
+sh apporun.sh
+rm apporun.sh
+
+if ! [ -d tracks_gap_with_cellid ]; then
+	mkdir tracks_gap_with_cellid
+fi
+
+
+for file in `ls $basedir/tracks_gap/*xml`; do
+
+	name=`echo $file | xargs -0 -n 1 basename | sed 's/\.tracks\.xml//g'`
+	
+	mask=`ls $pathmask/$name`
+	echo "nice -n 19 $pathfiji --ij2 --headless --run $pathspt/source/spot_detection_tracking/assign_cellids.py 'basedir=\"$basedir/tracks_gap_with_cellid\",xml=\"$file\",labeledFrames=\"93\",maskfile=\"$mask\"'" >> apporun1.sh
+
+done
+
+sh apporun1.sh
+rm apporun1.sh
