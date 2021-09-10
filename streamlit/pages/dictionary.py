d71e0b6ba6838acaafeb08cc0a7340c733ebd3a9
yinxiu zhan <yinxiuzhan@F346M-BD8E6A.local>
2021-09-10 09:18:30 +0200

    :sparkles: Add laura data, translation and rototranslation zhan implementation

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,6 +1,9 @@
 LIST_SAMPLES = {
     "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
+    "rad21_rigid": "https://drive.google.com/uc?export=download&id=1iaRZpaJTzX9OZplMDgMr8uvQuHaw9I9y",
+    "rad21_rototranslation": "https://drive.google.com/uc?export=download&id=1bN1fOQCtJrm991LvLHLKXCluKF_BOr8X",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
+    "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
     #    "directionality": "https://drive.google.com/uc?export=download&id=1WyP8p82md9HMZLQaqTnV9Hnt5ymGUpKx",
 }
 
@@ -15,4 +18,10 @@ PAGES = {
     "Directionality analysis": "direction",
 }
 
-SYSTEMATIC_ERRORS = {"rad21": 0.0087, "rousetime": 0.0025}
+SYSTEMATIC_ERRORS = {
+    "rad21": 0.0087,
+    "rousetime": 0.0025,
+    "rad21_rigid": 0.0087,
+    "rad21_rototranslation": 0.0087,
+    "lauradata": 0.0,
+}
