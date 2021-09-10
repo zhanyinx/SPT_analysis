7fe655da6325476955a973bcd77bab1d151958e4
yinxiu zhan <yinxiuzhan@F346M-BD8E6A.local>
2021-09-10 09:26:17 +0200

    :pencil: renaming translation data

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,6 +1,6 @@
 LIST_SAMPLES = {
     "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
-    "rad21_rigid": "https://drive.google.com/uc?export=download&id=1iaRZpaJTzX9OZplMDgMr8uvQuHaw9I9y",
+    "rad21_translation": "https://drive.google.com/uc?export=download&id=1iaRZpaJTzX9OZplMDgMr8uvQuHaw9I9y",
     "rad21_rototranslation": "https://drive.google.com/uc?export=download&id=1bN1fOQCtJrm991LvLHLKXCluKF_BOr8X",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
     "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
@@ -21,7 +21,7 @@ PAGES = {
 SYSTEMATIC_ERRORS = {
     "rad21": 0.0087,
     "rousetime": 0.0025,
-    "rad21_rigid": 0.0087,
+    "rad21_translation": 0.0087,
     "rad21_rototranslation": 0.0087,
     "lauradata": 0.0,
 }
