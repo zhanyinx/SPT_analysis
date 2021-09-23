fbba1424c105e2ad9c6862cdb35bdd5e8b0fc5a0
zhanyinx <yinxiu.zhan@fmi.ch>
2021-09-23 16:01:26 +0200

    :sparkles: Add track matching across channels pipeline

---
+++ source/assign_track_channels/assign_tracks.py
@@ -0,0 +1,97 @@
+import glob
+import os
+import numpy as np
+import pandas as pd
+import argparse
+import re
+
+from utils import *
+
+from dask.distributed import Client
+
+
+def _parse_args():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        default=None,
+        required=True,
+        help="Input folder to trajectories file from 2 channels.",
+    )
+    parser.add_argument(
+        "-d",
+        "--distance_cutoff",
+        type=float,
+        default=2.0,
+        required=False,
+        help="Maximum distance between trajectories to be considered as matching.",
+    )
+    parser.add_argument(
+        "-ml",
+        "--min_length",
+        type=int,
+        default=10,
+        required=False,
+        help="Minimum number of timepoints per trajectory.",
+    )
+    parser.add_argument(
+        "-c",
+        "--cost",
+        dest="cost",
+        action="store_true",
+        help="If defined, it will apply cost to partially overlapping tracks. See util.calculate_single_dist for more info",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default=None,
+        required=False,
+        help="Output folder. Default pair_wise_distance within the input folder.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """Match tracks from channels."""
+    # Parse input
+    args = _parse_args()
+
+    client = Client()
+
+    channel1_files = sorted(glob.glob(f"{args.input}/*w1*csv"))
+    names = [re.search(r"(^.*)w1", os.path.basename(x))[1] for x in channel1_files]
+    channel2_files = [glob.glob(f"{args.input}/{name}*w2*csv")[0] for name in names]
+
+    outdir = args.output
+    if args.output is None:
+        outdir = f"{args.input}/pair_wise_distance/"
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    for channel1, channel2 in zip(channel1_files, channel2_files):
+        outname = os.path.basename(channel1).replace("w1", "w1.w2")
+        channel1 = pd.read_csv(channel1)
+        channel1 = channel1[[X, Y, Z, FRAME, TRACKID, CELLID]]
+        channel2 = pd.read_csv(channel2)
+        channel2 = channel2[[X, Y, Z, FRAME, TRACKID, CELLID]]
+
+        channel2 = filter_tracks(channel2, min_length=args.min_length)
+        channel1 = filter_tracks(channel1, min_length=args.min_length)
+        res = merge_channels(
+            channel1, channel2, cost=args.cost, distance_cutoff=args.distance_cutoff
+        )
+        res = calculate_pairwise_distance(res)
+
+        res.to_csv(f"{outdir}/{outname}", index=False)
+
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ source/assign_track_channels/utils.py
@@ -0,0 +1,153 @@
+import dask
+import numpy as np
+import pandas as pd
+import scipy.optimize
+import secrets
+
+TRACKID = "TRACK_ID"
+X = "POSITION_X"
+Y = "POSITION_Y"
+Z = "POSITION_Z"
+FRAME = "FRAME"
+CELLID = "CELLID"
+
+
+def filter_tracks(df: pd.DataFrame, min_length: int = 10) -> pd.DataFrame:
+    """Filter tracks based on length.
+
+    Arg:
+       df: dataframe containing the tracked data
+       min_length: integer specifying the min track length
+
+    Return:
+       filtered data frame."""
+
+    distribution_length = df[TRACKID].value_counts()
+    selection = distribution_length.index.values[
+        distribution_length.values > min_length
+    ]
+
+    df = df[df[TRACKID].isin(selection)]
+    return df
+
+
+def calculate_single_dist(sub_df1, sub_df2, cost=True):
+    min_len = np.min([len(sub_df1), len(sub_df2)])
+    merged = pd.merge(sub_df1, sub_df2, how="inner", on=[FRAME])
+    if not len(merged):
+        return 9999999999
+    s1 = merged[[x + "_x" for x in [X, Y, Z]]].values
+    s2 = merged[[x + "_y" for x in [X, Y, Z]]].values
+    dist = np.mean(np.sum((s1 - s2) ** 2, axis=1)) / np.sqrt(len(merged))
+    if cost:
+        scaling = np.mean([len(sub_df1), len(sub_df2)]) / len(merged)
+        dist *= scaling
+    return dist
+
+
+@dask.delayed
+def calculate_dist(sub_df1, df2, cost=True):
+    dist = []
+    for _, sub_df2 in df2.groupby(TRACKID):
+        dist.append(calculate_single_dist(sub_df1, sub_df2, cost=cost))
+    return dist
+
+
+def calculate_distance(df1: pd.DataFrame, df2: pd.DataFrame, cost: bool = True):
+    """Return the matrix of distances between tracks in true and pred.
+    Since we want to privilege long overlapping tracks, we will divide the average distance by the
+    square-root of the number of overlapping points.
+
+    Args:
+        df1: dataframe containing the first dataset (ground truth, channel 1 etc..)
+        df2: dataframe containing the second dataset (ground truth, channel 2 etc..)
+        cost: if defined, the distance will be scaled by the fraction of no overlapping points
+    Return:
+        matrix of all pairwise distances.
+    """
+
+    dist = []
+    column_length = df1[TRACKID].nunique()
+    row_length = df2[TRACKID].nunique()
+    for _, sub_df1 in df1.groupby(TRACKID):
+        dist.append(calculate_dist(sub_df1, df2, cost=cost))
+
+    dist = dask.compute(dist)
+    dist = np.array(dist)
+
+    return dist
+
+
+def merge_channels(
+    df1: pd.DataFrame, df2: pd.DataFrame, cost: bool = True, distance_cutoff: int = 2
+):
+    """Return data frame containing the merged channel tracks
+
+    Args:
+        df1: dataframe containing the first dataset (ground truth, channel 1 etc..)
+        df2: dataframe containing the second dataset (ground truth, channel 2 etc..)
+        cost: if defined, the distance will be scaled by the fraction of no overlapping points
+        distance_cutoff: cutoff on the average distance between tracks to be considered as corresponding track.
+    Return:
+        data frame of merged datasers.
+    """
+
+    dist = calculate_distance(df1, df2, cost)
+    dist = dist.squeeze()
+
+    rows, cols = scipy.optimize.linear_sum_assignment(dist)
+    remove = 0
+    for r, c in zip(rows, cols):
+        if dist[r, c] > distance_cutoff:
+            rows = rows[rows != r]
+            cols = cols[cols != c]
+            remove += 1
+
+    track_ids_df1 = []
+    for trackid, _ in df1.groupby(TRACKID):
+        track_ids_df1.append(trackid)
+
+    track_ids_df2 = []
+    for trackid, _ in df2.groupby(TRACKID):
+        track_ids_df2.append(trackid)
+
+    track_list_df1 = np.array([track_ids_df1[i] for i in rows])
+    track_list_df2 = np.array([track_ids_df2[i] for i in cols])
+
+    results = pd.DataFrame()
+
+    for idx1, idx2 in zip(track_list_df1, track_list_df2):
+        sub1 = df1[df1[TRACKID] == idx1].copy()
+        sub2 = df2[df2[TRACKID] == idx2].copy()
+        tmp = pd.merge(sub1, sub2, on=FRAME, how="outer").sort_values(FRAME)
+        tmp["uniqueid"] = secrets.token_hex(16)
+        results = pd.concat([results, tmp])
+
+    return results
+
+
+def calculate_pairwise_distance(df: pd.DataFrame):
+    """Return pairwise distance trajectories.
+
+    Args:
+        df: DataFrame containing the matched tracks across two channels.
+
+    Return:
+        DataFrame of pairwise distance trajectories."""
+    df = df.dropna()
+    channel1 = df[[x + "_x" for x in [X, Y, Z]]].values
+    channel2 = df[[x + "_y" for x in [X, Y, Z]]].values
+
+    res = pd.DataFrame(channel1 - channel2)
+    res.columns = ["x", "y", "z"]
+    res["frame"] = df[FRAME].values
+    res["cell"] = [
+        f"w1.{w1}_w2.{w2}"
+        for w1, w2 in zip(df[CELLID + "_x"].values, df[CELLID + "_y"].values)
+    ]
+    res["track"] = [
+        f"w1.{w1}_w2.{w2}"
+        for w1, w2 in zip(df[TRACKID + "_x"].values, df[TRACKID + "_y"].values)
+    ]
+
+    return res
