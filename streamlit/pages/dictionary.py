20511226d0516d1fd5f90c0e6a64114afb45176e
Yinxiu Zhan <yinxiu.zhan@fmi.ch>
2021-10-13 21:37:14 +0200

    :zap: Update rad21 dataset

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,7 +1,6 @@
 LIST_SAMPLES_MSD = {
     "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
-    "rad21_translation": "https://drive.google.com/uc?export=download&id=1iaRZpaJTzX9OZplMDgMr8uvQuHaw9I9y",
-    "rad21_rototranslation": "https://drive.google.com/uc?export=download&id=1bN1fOQCtJrm991LvLHLKXCluKF_BOr8X",
+    "rad21_zhan_rototranslation": "https://drive.google.com/uc?export=download&id=1jPzZlfdJZj1Y33-q4Ev9_QB9yyLbzaYI",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
     "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
     "twocolor_degron": "https://drive.google.com/uc?export=download&id=1E-W_4G2mfSWRuPz6POL1CY29Cqkccj0-"
@@ -28,8 +27,7 @@ PAGES = {
 SYSTEMATIC_ERRORS = {
     "rad21": 0.0087,
     "rousetime": 0.0025,
-    "rad21_translation": 0.0087,
-    "rad21_rototranslation": 0.0087,
+    "rad21_zhan_rototranslation": 0.0087,
     "lauradata": 0.0,
     "twocolor_degron": 0.0,
 }
