38e76c222582ce691a978e41b5534546cf582c9e
Yinxiu Zhan <yinxiu.zhan@fmi.ch>
2021-10-27 11:28:12 +0200

    :sparkles: Remove Rad21 from Jan roto translation; Add two color imaging 30s dataset

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,16 +1,18 @@
 LIST_SAMPLES_MSD = {
-    "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
-    "rad21_zhan_rototranslation": "https://drive.google.com/uc?export=download&id=1jPzZlfdJZj1Y33-q4Ev9_QB9yyLbzaYI",
+    # "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
+    "rad21": "https://drive.google.com/uc?export=download&id=1jPzZlfdJZj1Y33-q4Ev9_QB9yyLbzaYI",
     "ctcf": "https://drive.google.com/uc?export=download&id=1y_hHx0vc7lN04kXpYpVkJVSlRiinOAex",
     "sororin_wapl": "https://drive.google.com/uc?export=download&id=16kvbu3ege8T-NnFje9yhThbxi04ImAb9",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
     "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
     "twocolor_degron": "https://drive.google.com/uc?export=download&id=125GzRSXLQelmkEVjxx-rEwCAewc9Yosu",
+    "twocolor_degron_30s": "https://drive.google.com/uc?export=download&id=1Atb_g-5uQnxfOF5RdcglJJqCEGpum_Am",
 }
 
 
 LIST_SAMPLES_PAIRWISE = {
-    "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1SHKH95xJLPHqoe6HLGLgHZCUhfwBp38s"
+    "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1SHKH95xJLPHqoe6HLGLgHZCUhfwBp38s",
+    "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1ZWwKzrwactyJ2VDbkpGGZLe2U8Ic2T1u",
 }
 
 PICKLED_DFS = {
@@ -30,7 +32,8 @@ SYSTEMATIC_ERRORS = {
     "ctcf": 0.0148,
     "sororin_wapl": 0.0148,
     "rousetime": 0.0025,
-    "rad21_zhan_rototranslation": 0.0148,
+    # "rad21_zhan_rototranslation": 0.0148,
     "lauradata": 0.0,
     "twocolor_degron": 0.0,
+    "twocolor_degron_30s": 0.0,
 }
