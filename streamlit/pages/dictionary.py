3dcd23d9ae6097414e5ca67c4e8910b5dbdaeabd
Yinxiu Zhan <yinxiu.zhan@fmi.ch>
2021-11-02 14:50:57 +0100

    :sparkles: Add two 2d fast acquisition data 1s from two color imaging

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -7,22 +7,26 @@ LIST_SAMPLES_MSD = {
     "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
     "twocolor_degron": "https://drive.google.com/uc?export=download&id=125GzRSXLQelmkEVjxx-rEwCAewc9Yosu",
     "twocolor_degron_30s": "https://drive.google.com/uc?export=download&id=1Atb_g-5uQnxfOF5RdcglJJqCEGpum_Am",
+    "twocolor_degron_2d_1s": "https://drive.google.com/uc?export=download&id=13w6A8RkDifMbG6lCL_7px-Gp9o9AJ7XU",
 }
 
 
 LIST_SAMPLES_PAIRWISE_DISTANCE = {
     "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1HCo7PWSkQjHi0i8F52A3-0wl5PBrdSn2",
     "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1NnOJCegnt_Ntkg7kaXoKdZg39KgAksFz",
+    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=18qBEGWNmv10pbWDeJvuW7YKKbiyvcO2X",
 }
 
 LIST_SAMPLES_DURATION = {
     "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1P6TtNueC-hCaF3jqPEg6OqJQhaLluZCi",
     "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1hI3SCcDNldBQBdcqGqeulB09FzdlcDVS",
+    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=1ZNrO_e-f_SqGB-R-Qp16GuUzxc3ah3rz",
 }
 
 LIST_SAMPLES_SECOND_PASSAGE_TIME = {
     "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1qwXtLFpX5PcN-20EYz-RP0XjambHMbNr",
     "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1Avq2hGk_Jmr2McJpSFmNj-cE-jC1Wz3Q",
+    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=1ImKpTc1LsoCxOiL30wXAAOHQja1_V-g1",
 }
 
 
@@ -47,4 +51,5 @@ SYSTEMATIC_ERRORS = {
     "lauradata": 0.0,
     "twocolor_degron": 0.0,
     "twocolor_degron_30s": 0.0,
+    "twocolor_degron_2d_1s": 0.0,
 }
