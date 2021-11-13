cdb61032e86bb94619613c6aa97162349b7d6c80
Yinxiu Zhan <yinxiu.zhan@fmi.ch>
2021-11-13 10:15:00 +0100

    :pencil: Add pairwise msd for rad21 dataset to the app

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,6 +1,7 @@
 LIST_SAMPLES_MSD = {
     # "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
     "rad21": "https://drive.google.com/uc?export=download&id=1jPzZlfdJZj1Y33-q4Ev9_QB9yyLbzaYI",
+    "rad21_pairwise": "https://drive.google.com/uc?export=download&id=1S1DW-a6o0Swx-UfzNTYVll_eJi5fMplW",
     "ctcf": "https://drive.google.com/uc?export=download&id=1y_hHx0vc7lN04kXpYpVkJVSlRiinOAex",
     "sororin_wapl": "https://drive.google.com/uc?export=download&id=16kvbu3ege8T-NnFje9yhThbxi04ImAb9",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
@@ -44,6 +45,7 @@ PAGES = {
 
 SYSTEMATIC_ERRORS = {
     "rad21": 0.0148,
+    "rad21_pairwise": 0.0119,
     "ctcf": 0.0148,
     "sororin_wapl": 0.0148,
     "rousetime": 0.0025,
