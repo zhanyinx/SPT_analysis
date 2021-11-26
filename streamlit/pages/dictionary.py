a5c65a130b9f7d00fa00e3abfb1977a7b70e2b4b
Yinxiu Zhan <yinxiu.zhan@fmi.ch>
2021-11-26 12:31:23 +0100

    :hammer: Refactoring dictionary and msd pairwise app

--- streamlit/pages/dictionary.py
+++ streamlit/pages/dictionary.py
@@ -1,40 +1,24 @@
 LIST_SAMPLES_MSD = {
-    # "rad21": "https://drive.google.com/uc?export=download&id=1UHfqftidv1MKOtnzhyXHNKVFQZPKIj2Z",
     "rad21": "https://drive.google.com/uc?export=download&id=1jPzZlfdJZj1Y33-q4Ev9_QB9yyLbzaYI",
     "rad21_pairwise": "https://drive.google.com/uc?export=download&id=1S1DW-a6o0Swx-UfzNTYVll_eJi5fMplW",
     "ctcf": "https://drive.google.com/uc?export=download&id=1y_hHx0vc7lN04kXpYpVkJVSlRiinOAex",
     "sororin_new_202111": "https://drive.google.com/uc?export=download&id=1301gA0rcPVTAj4GfMkKqZctMgqlEsxq8",
     "wapl_new_202111": "https://drive.google.com/uc?export=download&id=10LpFBdZrPaK4ACMsql992NQuWBO-mLKP",
-    "sororin_wapl": "https://drive.google.com/uc?export=download&id=16kvbu3ege8T-NnFje9yhThbxi04ImAb9",
     "rousetime": "https://drive.google.com/uc?export=download&id=1aMj2c6TkIN3NfKmRzD6-bCjcUz424Mf8",
-    # "lauradata": "https://drive.google.com/uc?export=download&id=1MAxNdi9zkOlZB-EHNlaZRlXAAEAX_fBT",
-    "twocolor_degron": "https://drive.google.com/uc?export=download&id=125GzRSXLQelmkEVjxx-rEwCAewc9Yosu",
-    "twocolor_degron_30s": "https://drive.google.com/uc?export=download&id=1Atb_g-5uQnxfOF5RdcglJJqCEGpum_Am",
-    "twocolor_degron_60s": "https://drive.google.com/uc?export=download&id=1NkR6GpsfoPkcMzPEjKvMwtRCaFBaQfBf",
-    "twocolor_degron_2d_1s": "https://drive.google.com/uc?export=download&id=13w6A8RkDifMbG6lCL_7px-Gp9o9AJ7XU",
-    "twocolor_degron_newlines": "https://drive.google.com/uc?export=download&id=1Z0gpCjrLRz1j7zx1CioBjB2ezcyqRxQu",
+    "twocolor_degron_all_lines_10s": "https://drive.google.com/uc?export=download&id=1n-Erm6jCL1GBq3vCsy5u1X_DFJjyot8I",
+    "twocolor_degron_twoctcf_2d_1s": "https://drive.google.com/uc?export=download&id=1AbztoKQy2V9kv9I5nnGAafhAVyjpAxNr",
+    "twocolor_degron_twoctcf_60s": "https://drive.google.com/uc?export=download&id=1Z7gc7hQEcKXZnGknvg2-A9UI1xmdX6Qz",
 }
 
 
 LIST_SAMPLES_PAIRWISE_DISTANCE = {
-    "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1HCo7PWSkQjHi0i8F52A3-0wl5PBrdSn2",
-    "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1NnOJCegnt_Ntkg7kaXoKdZg39KgAksFz",
-    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=18qBEGWNmv10pbWDeJvuW7YKKbiyvcO2X",
-    "twocolor_degron_pairwise_newlines": "https://drive.google.com/uc?export=download&id=1ydu_Ny7k06wTJmfUEkYXPMRJst94tHhI",
+    "twocolor_degron_pairwise_10s": "https://drive.google.com/uc?export=download&id=1BYeKZI7EXPb5-MtXbbXDFNFokG67rfo0",
+    # "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=18qBEGWNmv10pbWDeJvuW7YKKbiyvcO2X",
 }
 
-LIST_SAMPLES_DURATION = {
-    "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1P6TtNueC-hCaF3jqPEg6OqJQhaLluZCi",
-    "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1hI3SCcDNldBQBdcqGqeulB09FzdlcDVS",
-    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=1ZNrO_e-f_SqGB-R-Qp16GuUzxc3ah3rz",
-    "twocolor_degron_pairwise_newlines": "https://drive.google.com/uc?export=download&id=1PDbzU_AlrqAlxLCFHtyyJPGPASGrqhOj",
-}
-
-LIST_SAMPLES_SECOND_PASSAGE_TIME = {
-    "twocolor_degron_pairwise": "https://drive.google.com/uc?export=download&id=1qwXtLFpX5PcN-20EYz-RP0XjambHMbNr",
-    "twocolor_degron_pairwise_30s": "https://drive.google.com/uc?export=download&id=1Avq2hGk_Jmr2McJpSFmNj-cE-jC1Wz3Q",
-    "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=1ImKpTc1LsoCxOiL30wXAAOHQja1_V-g1",
-    "twocolor_degron_pairwise_newlines": "https://drive.google.com/uc?export=download&id=1qylpW8LVCHxr8MiRJs2OhXvJ29Dxi858",
+LIST_SAMPLES_MODEL = {
+    "twocolor_degron_pairwise_10s": "https://drive.google.com/uc?export=download&id=18f5yQTlRzfik_Dd0naacQvy6O5RwTzMa",
+    # "twocolor_degron_pairwise_2d_1s": "https://drive.google.com/uc?export=download&id=1ZNrO_e-f_SqGB-R-Qp16GuUzxc3ah3rz",
 }
 
 
@@ -56,12 +40,8 @@ SYSTEMATIC_ERRORS = {
     "ctcf": 0.0148,
     "sororin_wapl": 0.0148,
     "sororin_new_202111": 0.0148,
-    "wapl_new_202111": 0.0148,
     "rousetime": 0.0025,
-    # "lauradata": 0.0,
-    "twocolor_degron": 0.0119,
-    "twocolor_degron_30s": 0.0119,
-    "twocolor_degron_60s": 0.0119,
-    "twocolor_degron_2d_1s": 0.0119,
-    "twocolor_degron_newlines": 0.0119,
+    "twocolor_degron_all_lines_10s": 0.0119,
+    "twocolor_degron_twoctcf_2d_1s": 0.0119,
+    "twocolor_degron_twoctcf_60s": 0.0119,
 }
